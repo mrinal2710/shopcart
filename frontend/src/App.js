eed7afe1a2b572f654543300b8973bb08534feec
Mrinal Mitra <mmitra@aligntech.com>
2021-01-21 01:48:49 +0530

    adding header,footer and styling prerequisites

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
-    <div className="App">
-      <h1>home</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>home</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
