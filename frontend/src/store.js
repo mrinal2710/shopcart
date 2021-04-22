7e8941962e6a4db82bd01bee79fe54bbc097f3d3
Mrinal Mitra <mmitra@aligntech.com>
2021-04-22 23:39:16 +0530

    Product screen and getting user details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,14 +7,15 @@ import {
 } from "./reducers/productReducers";
 
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
-  userRegister: userRegisterReducer
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
