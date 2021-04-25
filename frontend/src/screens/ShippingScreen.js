7dfd7e2af58e6cf0c921f95856ad0ac3b30d9cc6
Mrinal Mitra <mmitra@aligntech.com>
2021-04-25 16:35:53 +0530

    Payment Screen and save payment

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux';
-import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../actions/cartActions';
 import CheckoutSteps from '../components/CheckoutSteps';
+import FormContainer from '../components/FormContainer';
 
 const ShippingScreen = ({ history }) => {
 
@@ -23,7 +23,7 @@ const ShippingScreen = ({ history }) => {
         history.push('/payment')
     }
     return (
-        <div>
+        <FormContainer>
             <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
@@ -72,7 +72,7 @@ const ShippingScreen = ({ history }) => {
                 </Button>
 
             </Form>
-        </div>
+        </FormContainer>
     )
 }
 
