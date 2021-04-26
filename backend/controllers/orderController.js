55ccc75703906d06189d5473f4a320ccc38de612
Mrinal Mitra <mmitra@aligntech.com>
2021-04-26 20:50:31 +0530

    getting an order by ID

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,27 +6,53 @@ import Order from "../models/orderModel.js";
 //@access Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
-    const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body
-    if (orderItems && orderItems.length === 0) {
-        res.status(400)
-        throw new Error('No order items')
-        return
-    } else {
-        const order = new Order({
-            orderItems,
-            user: req.user._id,
-            shippingAddress,
-            paymentMethod,
-            itemsPrice,
-            taxPrice,
-            shippingPrice,
-            totalPrice
-        })
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice
+  } = req.body;
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice
+    });
 
-        const createdOrder = await order.save()
+    const createdOrder = await order.save();
 
-        res.status(201).json(createdOrder)
-    }
+    res.status(201).json(createdOrder);
+  }
 });
 
-export { addOrderItems }
+//@desc Get Order by ID
+//@route GET /api/orders/:id
+//@access Private
+
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
